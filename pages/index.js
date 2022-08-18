3b02bc55795dcbd74adad8262e0a62b2b24ac20a
Josuisme <josucortez@hotmail.com>
2022-08-18 18:06:48 -0500

    updates

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import BoxTextSkills from './Components/BoxTextSkills'
+import Nav1 from './Components/Nav1'
 
 export default function Home() {
   return (
+    <div>
+      <Nav1/>
+
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <BoxTextSkills/>
+    </div>
   )
 }
