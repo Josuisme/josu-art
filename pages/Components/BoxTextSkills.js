3334c636fdbe981b2d9eb933f746e9b1ebeb5fcf
Josuisme <josucortez@hotmail.com>
2022-08-18 19:22:15 -0500

    Update

--- pages/Components/BoxTextSkills.js
+++ pages/Components/BoxTextSkills.js
@@ -1,38 +1,54 @@
-import React from 'react'
+import React from "react";
 
 const BoxTextSkills = () => {
   return (
-<div className="py-16 bg-yellow-100">  
-        <div className="container m-auto px-6 text-gray-500 md:px-12 xl:px-0">
-          <div className="mx-auto grid gap-6 md:w-3/4 lg:w-full lg:grid-cols-3">
-            <div className="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
-              <div className="mb-12 space-y-4">
-                <h3 className="text-2xl font-semibold text-purple-900">Graphic Design</h3>
-                <p className="mb-6">Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa iste officia sed officiis! Incidunt ea animi officiis.</p>
-                <a href="#" className="block font-medium text-purple-600">Know more</a>
-              </div>
-              imagen
+    <div className="py-16 bg-yellow-100">
+      <div className="text-center text-3xl p-4">Habilidades</div>
+      <div className="container m-auto px-6 text-gray-500 md:px-12 xl:px-0">
+        <div className="mx-auto grid gap-6 md:w-3/4 lg:w-full lg:grid-cols-3">
+          <div className="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
+            <div className="mb-12 space-y-4">
+              <h3 className="text-2xl font-semibold text-purple-900">
+                Diseño 3d
+              </h3>
+              <p className="mb-6">Puedo crear diseños 3d a pedido.</p>
+              <a href="#" className="block font-medium text-purple-600">
+                ver mas...
+              </a>
             </div>
-            <div className="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
-              <div className="mb-12 space-y-4">
-                <h3 className="text-2xl font-semibold text-purple-900">UI Design</h3>
-                <p className="mb-6">Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa iste officia sed officiis! Incidunt ea animi officiis.</p>
-                <a href="#" className="block font-medium text-purple-600">Know more</a>
-              </div>
-              imagen
+            imagen
+          </div>
+          <div className="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
+            <div className="mb-12 space-y-4">
+              <h3 className="text-2xl font-semibold text-purple-900">
+                Venta de diseños
+              </h3>
+              <p className="mb-6">Tengo en venta diseños sobre impresion 3d.</p>
+              <a href="#" className="block font-medium text-purple-600">
+                ver mas...
+              </a>
             </div>
-            <div className="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
-              <div className="mb-12 space-y-4">
-                <h3 className="text-2xl font-semibold text-purple-900">UX Design</h3>
-                <p className="mb-6">Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa iste officia sed officiis! Incidunt ea animi officiis.</p>
-                <a href="#" className="block font-medium text-purple-600">Know more</a>
-              </div>
-              imagen
+            imagen
+          </div>
+          <div className="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
+            <div className="mb-12 space-y-4">
+              <h3 className="text-2xl font-semibold text-purple-900">
+                Photoshop Ilustrador
+              </h3>
+              <p className="mb-6">
+                Retoque de fotografias: quitar fondo a imagenes, correccion de
+                colores.
+              </p>
+              <a href="#" className="block font-medium text-purple-600">
+                ver mas...
+              </a>
             </div>
+            imagen
           </div>
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default BoxTextSkills
+export default BoxTextSkills;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import BoxTextSkills from './Components/BoxTextSkills'
-import Nav1 from './Components/Nav1'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import BoxTextSkills from "./Components/BoxTextSkills";
+import Nav1 from "./Components/Nav1";
 
 export default function Home() {
   return (
     <div>
-      <Nav1/>
+      <Nav1 />
 
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <BoxTextSkills/>
+      <h1 className="text-3xl font-bold underline text-center bg-yellow-200 p-5">
+        Josu Cortez
+      </h1>
+      <BoxTextSkills />
+      <picture>
+        <source
+          srcSet="https://ep01.epimg.net/elpais/imagenes/2021/12/10/album/1639143803_433213_1639745607_noticia_normal.jpg"
+          type="image/webp"
+        />
+        <img
+          src="https://ep01.epimg.net/elpais/imagenes/2021/12/10/album/1639143803_433213_1639745607_noticia_normal.jpg"
+          alt="Landscape picture"
+        />
+      </picture>
     </div>
-  )
+  );
 }
