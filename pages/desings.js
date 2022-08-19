e0f1d85dafe41b7e384ca774a25c20abe9013bd2
Josuisme <josucortez@hotmail.com>
2022-08-19 08:54:33 -0500

    update

--- pages/desings.js
+++ pages/desings.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BoxText, Text } from "./Components/BoxText";
 import ContactMe from "./Components/ContactMe";
 import ImagenGal from "./Components/ImagenGal";
 import Nav1 from "./Components/Nav1";
@@ -12,9 +11,12 @@ const desings = () => {
       <h1 className="text-3xl font-bold underline text-center bg-yellow-200 p-5">
         Diseños 3d
       </h1>
-      <BoxText titulo="Los diseños 3d se utilizan en muchos lugares" />
-      <Text texto="Los diseños 3d son fundamentales para muchos proyectos, en los cuales no se puede tener el objeto fisicamente. Es por eso que se han convertido en un inpresindible en el cine, es con lo que se hacen muchos videojuegos y en general el entretenimiento siempere ha tenido en su mira a estos objetos creados por computadora." />
-
+      <h3 className="text-4xl text-center m-9 p-6 w-auto bg-amber-300 text-gray-800 hover:text-white">
+        Titulo
+      </h3>
+      <p className="font-semibold text-justify text-2xl m-20 p-5 h-auto w-auto bg-amber-100 text-gray-800 border-4 border-rose-600 rounded-3xl content-center bg-gradient-to-t from-amber-200 via-amber-200 flex-1">
+        Texto
+      </p>
       <ImagenGal />
       <ContactMe />
     </div>
